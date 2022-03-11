04325e050515dc05a9254890be0a95201957041f
e-hennep <e-hennep@asvg.no>
2022-03-11 23:01:54 +0100

    Basic setup of project

---
+++ Game-API/Game-API/Controllers/ScoreController.cs
@@ -0,0 +1,16 @@
+﻿using Game_API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Game_API.Controllers;
+
+[ApiController]
+[Route("score")]
+public class ScoreController : Controller
+{
+    private readonly IScoreService _scoreService;
+
+    public ScoreController(IScoreService scoreService)
+    {
+        _scoreService = scoreService;
+    }
+}
---
+++ Game-API/Game-API/Services/ScoreService.cs
@@ -0,0 +1,8 @@
+﻿using Game_API.Interfaces;
+
+namespace Game_API.Services;
+
+public class ScoreService : IScoreService
+{
+    
+}
